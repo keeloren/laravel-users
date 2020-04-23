b8e47cd936109c2f91686421063aaf78f50c6337
keeloren <keezimin@gmail.com>
2020-04-23 14:30:32 +0700

    Logout, resetpassword, change password

---
+++ src/Http/Controllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NguyenND\Users\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use NguyenND\Users\Traits\ResponseTrait;
+
+class BaseController extends Controller
+{
+    use ResponseTrait;
+    
+}
--- src/Routes/route.php
+++ src/Routes/route.php
@@ -2,41 +2,14 @@
 Route::group(['prefix' => 'api/users'], function () {
     
     Route::group(['namespace' => 'NguyenND\Users\Http\Controllers'], function () {
-//        Route::get('/laravel-user', 'AuthController@index');
         Route::post('register', 'AuthController@register');
         Route::post('oauth/token', 'AccessTokenController@issueToken');
+        Route::post('password/forgot/request', 'ForgotPasswordController@getResetToken');
+        Route::post('password/forgot/reset', 'ResetPasswordController@reset');
         Route::group(['middleware' => 'auth:api'], function () {
             Route::get('me', 'AuthController@authenticated');
+            Route::post('logout', 'AuthController@logout');
+            Route::post('password/change', 'ChangePasswordController@changePassword');
         });
-        
-        // Customer
-//        Route::post('customer/login', 'SessionController@create');
-//        Route::post('customer/register', 'CustomerController@create');
-//        Route::group(['middleware' => ['jwt.auth']], function () {
-//            Route::post('customer/forgot-password', 'ForgotPasswordController@store');
-//
-//            // Category
-//            Route::get('categories', 'ResourceController@index')->defaults('_config', [
-//                'repository' => 'Webkul\Category\Repositories\CategoryRepository',
-//                'resource' => 'Webkul\API\Http\Resources\Catalog\Category'
-//            ]);
-//
-//            // Card
-//            Route::get('/user/cards', 'CardController@cards');
-//            Route::get('/user/cards/{id}', 'CardController@showCard');
-//            Route::post('/user/cards', 'CardController@addCard');
-//            Route::patch('/user/cards/{id}', 'CardController@updateCard');
-//
-//            // Cart
-//            Route::group(['prefix' => 'checkout'], function () {
-//                Route::post('cart/add/{id}', 'CartController@store');
-//                Route::post('save-address', 'CheckoutController@saveAddressToCart');
-//                Route::post('save-shipping', 'CheckoutController@saveShipping');
-//                Route::post('save-payment', 'CheckoutController@savePayment');
-//            });
-//
-//            // Payment
-//            Route::post('/payment/{orderId}', 'PaymentController@store');
-//        });
     });
 });
